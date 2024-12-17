978e03c01ed3fee03ae3c67f47e96b51c8f492dd
khushijaiswal <jaiswalkhushi9011@gmail.com>
2024-12-17 09:59:24 +0530

    add footer

---
+++ dist/assets/DashBoard-DrpxhgV7.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-CzqpCvZk.js";const a=()=>r.jsx("div",{children:"DashBoard"});export{a as default};
---
+++ dist/assets/UserProfile-BWuhkYfx.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-CzqpCvZk.js";const s=()=>r.jsx("div",{children:"UserProfile"});export{s as default};
